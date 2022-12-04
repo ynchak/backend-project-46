35600ba33d00bac649e3d9826e707000cb2ac101
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2022-12-04 22:17:48 +0200

    add new module for build diff

---
+++ src/makeDiff.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+
+const makeDiff = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
+  const diff = keys.flatMap((key) => {
+    if (!_.has(data1, key)) {
+      return { status: 'added', key, value: data2[key] };
+    }
+    if (!_.has(data2, key)) {
+      return { status: 'removed', key, value: data1[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { status: 'nested', key, children: makeDiff(data1[key], data2[key]) };
+    }
+    if (data1[key] === data2[key]) {
+      return { status: 'unchanged', key, value: data1[key] };
+    }
+    return [
+      { status: 'removed', key, value: data1[key] },
+      { status: 'added', key, value: data2[key] },
+    ];
+  });
+  return diff;
+};
+export default makeDiff;
