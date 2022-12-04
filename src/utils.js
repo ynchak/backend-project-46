17b7d7462faf6a4ca266e68a007eed1c5f51fa0d
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2022-12-04 16:34:10 +0200

    update func name from getFile to getFilePath

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ import { readFileSync } from 'node:fs';
 import path from 'node:path';
 
 const getData = (file) => JSON.parse(readFileSync(file));
-const getFile = (pathToFile) => path.resolve(process.cwd(), pathToFile);
+const getFilePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
 
-export { getData, getFile };
+export { getData, getFilePath };
