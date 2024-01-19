49d434a8510fd8c93df5f800ae07a191fa9276d3
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-19 18:55:54 +0200

    complete test for flat json

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,1 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
+
 import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  join(__dirname, '..', '__fixtures__', filename);
+let result;
+beforeEach(() => {
+  const resultFile = getFixturePath('resultFlat.txt');
+  result = readFileSync(resultFile, 'utf8');
+});
+test('flat json test', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  expect(genDiff(file1, file2)).toBe(result);
+});
