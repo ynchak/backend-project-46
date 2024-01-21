c6f3c26f90f37c1c8df103ac1462fa160fe3b6d6
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-22 01:21:59 +0200

    update

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -10,21 +10,20 @@ const has = (obj, prop) => Object.hasOwn(obj, prop);
 const buildDiff = (before, after) => {
   const allKeys = [...Object.keys(before), ...Object.keys(after)];
   const uniqKeys = [...new Set(allKeys)].toSorted();
-  const diff = uniqKeys.reduce((acc, key) => {
+  return uniqKeys.reduce((diff, key) => {
     const valueBefore = before[key];
     const valueAfter = after[key];
     if (!has(before, key) && has(after, key)) {
-      return { ...acc, [key]: { status: 'added', valueAfter } };
+      return { ...diff, [key]: { status: 'added', valueAfter } };
     }
     if (has(before, key) && !has(after, key)) {
-      return { ...acc, [key]: { status: 'removed', valueBefore } };
+      return { ...diff, [key]: { status: 'removed', valueBefore } };
     }
     if (valueBefore === valueAfter) {
-      return { ...acc, [key]: { status: 'unchanged', valueBefore } };
+      return { ...diff, [key]: { status: 'unchanged', valueBefore } };
     }
-    return { ...acc, [key]: { status: 'updated', valueBefore, valueAfter } };
+    return { ...diff, [key]: { status: 'updated', valueBefore, valueAfter } };
   }, {});
-  return diff;
 };
 
 export default buildDiff;
