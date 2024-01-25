219c312f2a378180b8f58001a789c16db3184c32
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-25 21:23:37 +0200

    set default format stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,14 +4,15 @@ import { program } from 'commander';
 import genDiff from '../index.js';
 
 const getDiff = (filepath1, filepath2) => {
-  console.log(genDiff(filepath1, filepath2));
+  const { format } = program.opts();
+  console.log(genDiff(filepath1, filepath2, format));
 };
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action(getDiff);
 
