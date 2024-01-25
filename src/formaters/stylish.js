447d5ae254889da298c626c100ff16693d0f829e
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-25 21:25:48 +0200

    refactor for nested structs

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,25 +1,48 @@
-const stylish = (diff) => {
-  const entries = Object.entries(diff);
-  const lines = entries.flatMap(
-    ([key, { status, valueBefore, valueAfter }]) => {
+import { isObject } from 'lodash-es';
+
+const stringify = (data, depth) => {
+  if (!isObject(data)) {
+    return `${data}`;
+  }
+  const lines = Object.entries(data).map(([key, value]) => {
+    const indent = ' '.repeat(4 * depth - 2);
+    return `${indent}  ${key}: ${stringify(value, depth + 1)}`;
+  });
+  const indent = ' '.repeat(4 * (depth - 1));
+  return ['{', ...lines, `${indent}}`].join('\n');
+};
+
+export default (data) => {
+  const stylish = (diff, depth) => {
+    const spaceCount = 4;
+    const indent = ' '.repeat(spaceCount * depth - 2);
+    const lastIndent = ' '.repeat(spaceCount * (depth - 1));
+    const entries = Object.entries(diff);
+    const lines = entries.flatMap(([key, { status, value, valueAfter }]) => {
       switch (status) {
         case 'added': {
-          return `  + ${key}: ${valueAfter}`;
+          return `${indent}+ ${key}: ${stringify(value, depth + 1)}`;
         }
         case 'removed': {
-          return `  - ${key}: ${valueBefore}`;
+          return `${indent}- ${key}: ${stringify(value, depth + 1)}`;
         }
         case 'unchanged': {
-          return `    ${key}: ${valueBefore}`;
+          return `${indent}  ${key}: ${stringify(value, depth + 1)}`;
         }
         case 'updated': {
-          return [`  - ${key}: ${valueBefore}`, `  + ${key}: ${valueAfter}`];
+          return [
+            `${indent}- ${key}: ${stringify(value, depth + 1)}`,
+            `${indent}+ ${key}: ${stringify(valueAfter, depth + 1)}`,
+          ];
+        }
+        case 'nested': {
+          return `${indent}  ${key}: ${stringify(stylish(value, depth + 1))}`;
         }
         default:
           throw new Error('');
       }
-    }
-  );
-  return ['{', ...lines, '}'].join('\n');
+    });
+    return ['{', ...lines, `${lastIndent}}`].join('\n');
+  };
+  return stylish(data, 1);
 };
-export default stylish;
