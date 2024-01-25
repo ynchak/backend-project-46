f9c8bb90ca649d5e6efc0e4287f3d74824ca3233
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-25 21:25:53 +0200

    refactor for nested structs

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,4 +1,4 @@
-const has = (obj, prop) => Object.hasOwn(obj, prop);
+import { has, isObject } from 'lodash-es';
 
 // const difference = {
 //   status: '',
@@ -14,15 +14,22 @@ const buildDiff = (before, after) => {
     const valueBefore = before[key];
     const valueAfter = after[key];
     if (!has(before, key) && has(after, key)) {
-      return { ...diff, [key]: { status: 'added', valueAfter } };
+      return { ...diff, [key]: { status: 'added', value: valueAfter } };
     }
     if (has(before, key) && !has(after, key)) {
-      return { ...diff, [key]: { status: 'removed', valueBefore } };
+      return { ...diff, [key]: { status: 'removed', value: valueBefore } };
     }
     if (valueBefore === valueAfter) {
-      return { ...diff, [key]: { status: 'unchanged', valueBefore } };
+      return { ...diff, [key]: { status: 'unchanged', value: valueBefore } };
     }
-    return { ...diff, [key]: { status: 'updated', valueBefore, valueAfter } };
+    if (isObject(valueBefore) && isObject(valueAfter)) {
+      const value = buildDiff(valueBefore, valueAfter);
+      return { ...diff, [key]: { status: 'nested', value } };
+    }
+    return {
+      ...diff,
+      [key]: { status: 'updated', value: valueBefore, valueAfter },
+    };
   }, {});
 };
 
