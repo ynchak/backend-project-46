cd361a1f135044926f140c598e8889dadbe8f9a8
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-28 14:39:09 +0200

    add fixtures for plain output format

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -9,19 +9,18 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) =>
   join(__dirname, '..', '__fixtures__', filename);
-let result;
-beforeEach(() => {
-  const resultFile = getFixturePath('result.txt');
-  result = readFileSync(resultFile, 'utf8');
-});
+
 
 it.each([
   ['json', 'json'],
   ['yml', 'yml'],
   ['json', 'json', 'stylish'],
   ['yml', 'yml', 'stylish'],
+  ['json', 'json', 'plain'],
+  ['yml', 'yml', 'plain'],
 ])('gendiff(%s, %s, %s)', (ext1, ext2, format = 'stylish') => {
   const pathTofile1 = getFixturePath(`file1.${ext1}`);
   const pathTofile2 = getFixturePath(`file2.${ext2}`);
+  const result = readFileSync(getFixturePath(`result.${format}.txt`), 'utf8');
   expect(genDiff(pathTofile1, pathTofile2, format)).toEqual(result);
 });
