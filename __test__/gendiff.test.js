9335c4d11312be0283d4d3a3d41aafb6b3b6d356
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-01-28 15:46:53 +0200

    add new formater json

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -10,7 +10,6 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) =>
   join(__dirname, '..', '__fixtures__', filename);
 
-
 it.each([
   ['json', 'json'],
   ['yml', 'yml'],
@@ -18,6 +17,8 @@ it.each([
   ['yml', 'yml', 'stylish'],
   ['json', 'json', 'plain'],
   ['yml', 'yml', 'plain'],
+  ['json', 'json', 'json'],
+  ['yml', 'yml', 'json'],
 ])('gendiff(%s, %s, %s)', (ext1, ext2, format = 'stylish') => {
   const pathTofile1 = getFixturePath(`file1.${ext1}`);
   const pathTofile2 = getFixturePath(`file2.${ext2}`);
--- src/formaters/index.js
+++ src/formaters/index.js
@@ -1,10 +1,12 @@
 import { has } from 'lodash-es';
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formats = {
   stylish,
   plain,
+  json,
 };
 export default (diff, format) => {
   if (!has(formats, format)) {
